edba0af6d6c87a09d5eb84fd82c1cdff560b7270
me-techshruti54212 <shrutiverma.me@gmail.com>
2024-06-07 01:23:40 +0530

    cartRoute added

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const {foodRouter}=require("./routes/foodRoute.js")
 const bodyParser=require('body-parser')
 const userRouter=require("./routes/userRoute.js")
 const dotenv=require("dotenv/config")
+const cartRouter = require("./routes/cartRoute.js")
 
 
 const app=express()
@@ -19,6 +20,7 @@ app.get("/",(req,res)=>res.send("Hello"))
 app.use("/api/food",foodRouter)
 app.use("/images",express.static(__dirname + '/uploads'))
 app.use("/api/user",userRouter)
+app.use("/api/cart",cartRouter)
 
 app.listen(process.env.PORT,()=>{`Server started at http://localhost:${process.env.PORT}`})
 
