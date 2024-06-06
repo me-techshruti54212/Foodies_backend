d529ac63b7976e55986f158dd894a64b59ec7aeb
me-techshruti54212 <shrutiverma.me@gmail.com>
2024-06-07 01:59:30 +0530

    cartRouter added

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -1,10 +1,11 @@
 const express=require("express")
 const {addToCart,removeFromCart,getCart}=require("../controllers/cartController")
+const {authMiddleware}=require("../middlewares/auth")
 
 const cartRouter=express.Router;
 
-cartRouter.post("/add",addToCart)
-cartRouter.post("/remove",removeFromCart)
-cartRouter.post("/get",getCart)
+cartRouter.post("/add",authMiddleware,addToCart)
+cartRouter.post("/remove",authMiddleware,removeFromCart)
+cartRouter.post("/get",authMiddleware,getCart)
 
 module.exports=cartRouter
