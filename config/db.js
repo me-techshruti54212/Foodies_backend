290ced466b7edb535d02796c59d6764d71591629
me-techshruti54212 <shrutiverma.me@gmail.com>
2024-06-07 00:45:29 +0530

    mongodb connected

--- config/db.js
+++ config/db.js
@@ -4,7 +4,6 @@ const connectDb=async()=>{
     
     await mongoose.connect(process.env.MONGODB_URL).then(()=>console.log("database connected")).catch((err)=>console.log(err))
     
-   
 }
 
 module.exports={connectDb}
