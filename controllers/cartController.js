e9cc94ffe27f537a533722a68c127f2c50fc8f31
me-techshruti54212 <shrutiverma.me@gmail.com>
2024-06-07 01:13:05 +0530

    info of items added to cart by user

---
+++ controllers/cartController.js
@@ -0,0 +1,15 @@
+const userModel=require("../models/userModel")
+
+const addToCart=async(req,res)=>{
+
+}
+
+const removeFromCart=async(req,res)=>{
+    
+}
+const getCart=async(req,res)=>{
+    
+}
+
+
+module.exports= {addToCart,removeFromCart,getCart}
