d42ab04cdd05a62333b27b806ab39934f078e8ad
me-techshruti54212 <shrutiverma.me@gmail.com>
2024-06-07 09:46:01 +0530

    uploads ignored

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,21 +1,21 @@
-import jwt from "jsonwebtoken"
+const jwt= require("jsonwebtoken")
 
 
 const authMiddleware=async(req,res)=>{
-const {token}=req.headers;
-if(!token)
-    {
-        return res.json({success:false,message:"Not Authorized"})
-    }
-    try{
-        const token_decode=jwt.verify(token,process.env.JWT_SECRET)
-        req.body.userId=token.decode.id;
-        next();
-    }
-    catch(err){
-        console.log(err);
-        res.json({success:false,message:"Error"})
-    }
+// const {token}=req.headers;
+// if(!token)
+//     {
+//         return res.json({success:false,message:"Not Authorized"})
+//     }
+// try{
+//         const token_decode=jwt.verify(token,process.env.JWT_SECRET)
+//         req.body.userId=token_decode.id;
+//         next();
+//     }
+//     catch(err){
+//         console.log(err);
+//         res.json({success:false,message:"Error"})
+//     }
 }
 
 module.exports={authMiddleware}
--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -2,10 +2,10 @@ const express=require("express")
 const {addToCart,removeFromCart,getCart}=require("../controllers/cartController")
 const {authMiddleware}=require("../middlewares/auth")
 
-const cartRouter=express.Router;
+const cartRouter=express.Router();
 
 cartRouter.post("/add",authMiddleware,addToCart)
 cartRouter.post("/remove",authMiddleware,removeFromCart)
 cartRouter.post("/get",authMiddleware,getCart)
 
-module.exports=cartRouter
+module.exports={cartRouter}
