483733614a83438fcb2b94b820bbdc1488edde88
me-techshruti54212 <shrutiverma.me@gmail.com>
2024-06-08 20:33:28 +0530

    user authentication done

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,21 +1,26 @@
 const jwt= require("jsonwebtoken")
 
 
-const authMiddleware=async(req,res)=>{
-// const {token}=req.headers;
-// if(!token)
-//     {
-//         return res.json({success:false,message:"Not Authorized"})
-//     }
-// try{
-//         const token_decode=jwt.verify(token,process.env.JWT_SECRET)
-//         req.body.userId=token_decode.id;
-//         next();
-//     }
-//     catch(err){
-//         console.log(err);
-//         res.json({success:false,message:"Error"})
-//     }
+const authMiddleware=async(req,res,next)=>{
+const token=req.headers["token"];
+// console.log(authHeader)
+// const [Bearer,token]=authHeader?.split(" ");
+// const token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2NjNlOGNhMTg4NzMyYTE3MTc0NzgwYiIsImlhdCI6MTcxNzgyMzY5MH0.dmRYzlFGFtpOuOPz2YeS3Lt7Y2jiN8q7ljoXqLzQix8"
+// console.log(token)
+if(!token)
+    {
+        return res.json({success:false,message:"Not Authorized"})
+    }
+try{
+        const token_decode=jwt.verify(token,process.env.JWT_SECRET)
+        // console.log(token_decode)
+        req.body.userId=token_decode.id;
+        next();
+    }
+    catch(err){
+        // console.log(err);
+        res.json({success:false,message:"Errorr"})
+    }
 }
 
 module.exports={authMiddleware}
