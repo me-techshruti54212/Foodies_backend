ab88e1da0c2d26d661342c3cc48a45b5b2d8f4eb
me-techshruti54212 <shrutiverma.me@gmail.com>
2024-06-08 20:35:13 +0530

    add ,remove ,get items from db

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,9 +1,10 @@
-const userModel=require("../models/userModel")
+const {userModel}=require("../models/userModel")
 
 const addToCart=async(req,res)=>{
+    // console.log(req.body.userId)
 try{
     let userData=await userModel.findOne({_id:req.body.userId})
-    let cartData=await userData.cartData
+    let cartData=await userData.cartData;
     if(!cartData[req.body.itemId])
         {
             cartData[req.body.itemId]=1;
@@ -12,7 +13,7 @@ try{
         else{
             cartData[req.body.itemId]+=1;
         }
-        await userModel.findByIdAndUpdate(req.body.userId,{cartData})
+        await userModel.findByIdAndUpdate(req.body.userId,{cartData:cartData})
         res.json({success:true,message:"Added to cart"})
 }
 catch(err)
@@ -23,11 +24,58 @@ res.json({success:false,message:"Error"})
 }
 
 const removeFromCart=async(req,res)=>{
-    
+    try{
+        let userData=await userModel.findOne({_id:req.body.userId})
+        let cartData=await userData.cartData;
+       
+      
+        if(cartData[req.body.itemId]>1){
+                cartData[req.body.itemId]-=1;
+               
+            }
+         
+            await userModel.findByIdAndUpdate(req.body.userId,{cartData})
+            res.json({success:true,message:"removed from cart"}) 
+    }
+    catch(err)
+    {
+    console.log(err)
+    res.json({success:false,message:"Error"})
+    }
 }
 const getCart=async(req,res)=>{
+    try{
+    let userData=await userModel.findOne({_id:req.body.userId})
+    let cartData=await userData.cartData;
+    res.json({success:true,cartData:cartData})
+    }
+    catch(err){
+        console.log(err)
+        res.json({success:false,message:"Error"})
+    }
+   
+}
+const removeAll=async(req,res)=>{
+    try{
+        let userData=await userModel.findOne({_id:req.body.userId})
+        console.log(userData)
+      
+        let myObjCopy = JSON.parse(JSON.stringify(userData));
+        delete myObjCopy.cartData[req.body.itemId];
+        const data=myObjCopy.cartData
+        await userModel.findByIdAndUpdate(req.body.userId,{cartData:data})
+
+      
+        }
+        catch(err){
+            console.log(err)
+            res.json({success:false,message:"Error"})
+        }
     
+
+
 }
 
 
-module.exports= {addToCart,removeFromCart,getCart}
+
+module.exports= {addToCart,removeFromCart,getCart,removeAll}
