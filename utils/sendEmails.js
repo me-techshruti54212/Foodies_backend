fe36958dd08e218ab806177a3989c887818bb6a4
me-techshruti54212 <shrutiverma.me@gmail.com>
2024-08-29 10:51:35 +0530

    image

--- utils/sendEmails.js
+++ utils/sendEmails.js
@@ -7,6 +7,7 @@ const sendEmail = async (email,otp) => {
         subject:`${otp} for Foodies`,
         html:`
         <div class="otpDiv" style=" width:80%; margin:auto; padding:10px">
+          <img src="https://foodies-backend-8h8p.onrender.com/images/foodieslogo2.png" />
             <h2 class="otpHead" style="color:#5C3F76;">Team Foodies</h2>
             <h3 style="margin-bottom:10px;">Thankyou for using Foodies</h3>
            <h1><b style="margin-bottom:20px; color:#5C3F76; font-size=30px;">${otp}</b></h1>
